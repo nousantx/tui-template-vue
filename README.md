3049f69b5785e6b64a84f94e471e7e0449107a49
nousantx <nousantx@gmail.com>
2024-04-15 09:53:10 +0700

    feat: Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,35 @@
 # vue-tui
 
 ## Project setup
+
 ```
 yarn install
 ```
 
+### Add TenoxUI
+
+```
+yarn add tenoxui -D
+```
+
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="center w-full h-mn-100vh p-2rem ph-10vw">
+    <header class="ta-center">
+      <h1 class="fs-2.4rem">Vue x TenoxUI App</h1>
+      <p class="mb-2rem tc-#aaa fs-14px">
+        Try tenoxui! It's fast, sometimes :D
+      </p>
+      <p class="tc-gray fs-12px">Edit <code>/src/App.vue</code></p>
+    </header>
+  </section>
+  <footer class="center w-full position-fixed b-0 p-1rem">
+    <p class="fs-12px tc-#aaa">&copy; 2024 NOuSantx. Built with tenoxui/css</p>
+  </footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from "vue";
+import tenoxui, { defineProps as makeProps, makeStyles } from "tenoxui";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    onMounted(() => {
+      // adding new props
+      makeProps({
+        // type example
+        d: "display",
+      });
+      // define styles
+      makeStyles({
+        // styler example
+        body: "bg-#0d0d0d tc-white family-[font-body]",
+        code: "bg-[code-back] ph-6px pv-2px br-0.125rem fs-inherit",
+        // reusable class example
+        ".center": "d-flex flex-parent-center",
+        ".w-full": "w-100%",
+      });
+      // init tenoxui
+      tenoxui();
+    });
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap");
+:root {
+  --font-body: "gabarito", sans-serif;
+  --primary: #ccf654;
+  --code-back: rgba(34, 34, 34, 0.3);
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
